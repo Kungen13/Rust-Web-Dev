f576e7a07c8e570dcc88840d230937b13b0ce99e
Taylor Pettingill <134963416+Kungen13@users.noreply.github.com>
2024-05-31 14:50:57 -0700

    get questions handler

--- src/handler.rs
+++ src/handler.rs
@@ -31,7 +31,47 @@ pub async fn server_checker_handler() -> impl IntoResponse {
     Json(json_response)
 }
 
-pub async fn question_list_handler(
+pub async fn question_list_handler (
+    opts: Option<Query<FilterOptions>>,
+    State(data): State<Arc<AppState>>,
+) -> Result<imple IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    let Query(opts) = opts.unwrap_or_default();
+
+    let limit = opts.limit.unwrap_or(10);
+    let offset = (opts.page.unwrap_or(1) - 1) * limit;
+
+    //Query
+    let questions = sqlx::query_as!(
+        Question,
+        r#"SELECT * FROM questions ORDER by id LIMIT ? OFFSET ?"#,
+        limit as i32,
+        offset as i32
+    )
+    .fetch_all(&data.db)
+    .await
+    .map_err(|e| {
+        let error_response = serde_json::json!({
+            "status": "error",
+            "message": format!("Database error: {}", e),
+        });
+        (StatusCode::INTERNAL_SERVER_ERROR, Json(error_response))
+    })?;
+
+    //Response
+    let question_responses = questions
+        .iter()
+        .map(|question| to_question_response(&question))
+        .collect::<Vec<QuestionResponse>>();
+
+    let json_response = serde_json::json!({
+        "status": "ok",
+        "count": question_responses.len(),
+        "notes": question_responses
+    });
+    Ok(Json(json_response))
+}
+
+/*pub async fn question_list_handler(
     options: Option<Query<QueryOptions>>,
     State(db): State<DB>,
 ) -> impl IntoResponse {
@@ -165,4 +205,4 @@ pub async fn delete_question_handler(
         "message": format!("Question with ID: {} not found", id)
     });
     Err((StatusCode::NOT_FOUND, Json(error_response)))
-}
+}*/
