de6514ee48ce9eb28e9dd030436a537cf21570a9
TaylorP_MSP9 <tapet2@pdx.edu>
2024-06-10 14:54:54 -0700

    adding folder for front + back end

--- migrations/20240531052327_create_questions_table.up.sql
+++
@@ -1,7 +0,0 @@
--- Add up migration script here
---command: sqlx migrate run
-CREATE TABLE IF NOT EXISTS questions (
-    id CHAR(36) PRIMARY KEY NOT NULL,
-    title VARCHAR(255) NOT NULL UNIQUE,
-    content TEXT NOT NULL
-);
